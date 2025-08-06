19a733a935f84b99b0cdc008995baf1499880220
Nathalya Silva <nathalya.gsn@gmail.com>
2025-08-06 17:30:03 -0300

    exercicios

---
+++ praticandoBiblioteca.py
@@ -0,0 +1,58 @@
+import matplotlib
+import numpy as np
+import random
+import math
+
+# lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+# escolha = random.choice(lista)
+# print(f"Número escolhido {escolha}")
+
+# numeroPositivoMenorQueCem = random.randrange(100)
+# print(numeroPositivoMenorQueCem)
+
+# numero1 = int(input("Digite o primeiro número: "))
+# numero2 = int(input("Digite o segundo número: "))
+
+# potencia = pow(numero1, numero2)
+
+# print(f"{numero1} elevado a {numero2} = {potencia}")
+
+# participantes = int(input("Quantos participantes? "))
+# escolha = random.randint(1, participantes)
+
+# print(f"O participante escolhido foi: {escolha}")
+
+# nome = str(input("Digite seu nome: "))
+# token = random.randrange(1000, 10000, 2)
+
+# print(f"Olá {nome}, seu token é {token}")
+
+# frutas = ["Mação", "Banana", "Uva", 
+#           "Pêra", "Manga", "Coco", 
+#           "Melancia", "Mamão", "Laranja", 
+#           "Abacaxi", "Kiwi", "Ameixa"]
+
+# frutasSelecionadas = 3
+
+# saladaDeFrutaSurpresa = random.sample(frutas, frutasSelecionadas)
+
+# print(f"As frutas da sua salada serão: {saladaDeFrutaSurpresa}")
+
+# numeros = [2, 8, 15, 23, 91, 112, 256]
+
+# for n in numeros:
+#     raiz = math.sqrt(n)
+#     if raiz // 1 == raiz:
+#         print(f"A raiz quadrada de {n} é {int(raiz)} ")
+
+# raio = float(input("Digite o raio do jardim em metros? "))
+# valordaGrama = 25.00
+
+# area = math.pi * math.pow(raio, 2)
+
+# valorTotal = area * valordaGrama
+
+# print(f"O custo total para um jardim de {raio} m² é de: R${valorTotal:.2f} ")
+
+
+
